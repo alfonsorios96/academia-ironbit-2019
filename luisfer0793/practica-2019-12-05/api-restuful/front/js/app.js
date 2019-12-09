6ff366524e670f093db2b7990e5b8e2e448f661f
Luis Fernando Jimenez Lopez <luisfer.0793@gmail.com>
2019-12-09 12:56:52 -0600

    Testing integration

--- luisfer0793/practica-2019-12-05/api-restuful/front/js/app.js
+++ luisfer0793/practica-2019-12-05/api-restuful/front/js/app.js
@@ -1,31 +1,39 @@
 // Botones.
 const [moviesBtn, usersBtn, newUserBtn] = document.querySelectorAll('.button');
+const grid = document.getElementById('grid');
 
 // Llamada fetch con el metodo HTTP - GET
 moviesBtn.addEventListener('click', event => {
     fetch('http://localhost:3000/movies')
         .then(response => response.json())
-        .then(movies => movies.forEach(movie => console.table(movie)))
+        .then(movies => movies.forEach(movie => grid.insertAdjacentHTML('beforeend', `
+            <div class="movie">
+                <img class="movie__img" src="${movie.imageURL}">
+                <h1>${movie.title}</h1>
+                <p>${movie.description}</p>
+            </div>
+        `)))
         .catch(error => console.log(error))
 });
 
 usersBtn.addEventListener('click', event => {
     fetch('http://localhost:3000/users')
         .then(data => data.json())
-        .then(users => users.forEach(user => console.table(user)))
+        .then(users => users.forEach(user => console.log(user)))
         .catch(error => console.log(error))
 });
 
 // Fetch, recibe una URL y un elemento objet opcional.
-const userExample = {
-    username: "example",
-    email: "example@example.com",
-    password: "examplepass"
+const newObj = {
+    "id": 1,
+    "username": "example",
+    "email": "example@example.com",
+    "password": "examplepass"
 };
 
 const options = {
     method: 'POST',
-    body: JSON.stringify(userExample),
+    body: JSON.stringify(newObj),
     headers: {
         'Content-Type': 'application/json'
     }
@@ -33,6 +41,7 @@ const options = {
 
 // Tal object será una colección de opciones qye debe cumplir la llamada a la URL.
 newUserBtn.addEventListener('click', event => {
+    event.preventDefault();
     fetch('http://localhost:3000/users', options)
         .then(response => console.log(response))
         .catch(error => console.log(error))
