396505202c5a2a321eaaa321d3b4da613be2ab3b
Luis Fernando Jimenez Lopez <luisfer.0793@gmail.com>
2019-12-10 17:39:51 -0600

    Object Oriented Practice

---
+++ luisfer0793/practica-2019-12-10/pojos/Escuela.js
@@ -0,0 +1,15 @@
+import Empleado from './Empleado.js';
+
+class Escuela {
+    constructor() {
+        Object.defineProperty(this, 'teachers', {value: [
+            new Empleado('Fernando', 15, 20),
+            new Empleado('Mar', 0, 30),
+            new Empleado('Luis', 5, 0),
+        ], writable: false})
+    }
+}
+
+const miEscuela = new Escuela();
+
+miEscuela.teachers.forEach(empleado => console.log(empleado.isBusy()));
