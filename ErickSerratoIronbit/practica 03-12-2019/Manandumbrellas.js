bcebb895698258835574406d9b5f4893d76aa849
ErickSerrato <Erick.Serrato@ironbit.com.mx>
2019-12-16 04:46:48 -0600

    feat (add docs this practice 041219)

--- ErickSerratoIronbit/practica 03-12-2019/Manandumbrellas.js
+++ ErickSerratoIronbit/practica 03-12-2019/Manandumbrellas.js
@@ -1,12 +1,23 @@
-const minUmbrellas = (weather) =>{
+const weater = ["sunny","clear","sunny","sunny","clear","clear","sunny","rainy","clear","clear","windy","clear","clear","windy","sunny","clear","cloudy","clear","clear","sunny","sunny","rainy","thunderstorms","sunny","clear","windy","thunderstorms","windy","windy","clear","windy","clear","clear","clear","windy","clear","windy","sunny","windy","clear","windy","sunny","sunny","sunny","windy","sunny","sunny","sunny","windy","windy","sunny","sunny","rainy","thunderstorms","rainy","rainy","clear","windy","windy","thunderstorms","windy","clear","windy","clear","sunny","windy","cloudy","sunny","clear","clear","clear","sunny","cloudy","clear","windy","rainy","windy","clear","clear","sunny","sunny","sunny","sunny","cloudy","clear","clear","sunny","windy","windy","windy","thunderstorms","windy","windy","clear","thunderstorms","sunny","sunny","sunny","sunny","clear"];
 
-  let totalumbrellaswork = 0;
-  let totalumbrellashome = 0;
+const numumbrellas =(weater) =>{
+  let totalnumberumbrellashome = 0;
+  let totalnumberumbrellaswork = 0;
+  let totalumbrellas = 0;
 
-  const days  = weather.map(
-    (day,index ) =>{
+  weater.reduce((totalumbrellas, weaters, index  ) => {
+    if(weaters === "rainy" || weaters === "thunderstorms" ){
+
+      if(index %2 ===0 ){
+        totalnumberumbrellashome +=1;
+        totalumbrellas +=1;
+      }else{
+        totalnumberumbrellaswork +=1;
+        totalumbrellas +=1;
+      }
 
     }
-  )
+    return totalumbrellas;
+  })
 
 }
