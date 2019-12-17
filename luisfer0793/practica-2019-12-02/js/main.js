aac1bca06e590a77a7ba147b6d9eea4a6c8f0290
Luis Fernando Jimenez Lopez <luisfer.0793@gmail.com>
2019-12-17 16:07:33 -0600

    Added some sass files

--- luisfer0793/practica-2019-12-02/js/main.js
+++ luisfer0793/practica-2019-12-02/js/main.js
@@ -1,5 +1,22 @@
 const loginBtn = document.getElementById('loginBtn');
+const movieBtn = document.getElementById('movieBtn');
+const movieGrid = document.querySelector('.grid');
 
 loginBtn.addEventListener('click', () => {
     location.href="./login.html";
+});
+
+movieBtn.addEventListener('click', event => {
+    fetch('http://localhost:3000/movies')
+        .then(response => response.json())
+        .then(data => data.forEach(movie => {
+            movieGrid.insertAdjacentHTML('beforeend', `
+            <div class="movie">
+                <img src="${movie.poster}" alt="movie poster" class="movie__img">
+                <h2 class="movie__title">${movie.title}</h2>
+                <p class="movie__description">${movie.description}</p>
+            </div>
+            `);
+        }))
+        .catch(error => console.error(error));
 });
