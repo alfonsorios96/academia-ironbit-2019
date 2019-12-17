a52c07fae7b744d2021999a86954e48cd6e3d3e8
Luis Fernando Jimenez Lopez <luisfer.0793@gmail.com>
2019-12-17 16:08:37 -0600

    Created LitElement component

---
+++ luisfer0793/practica-2019-12-17/ib-login.js
@@ -0,0 +1,61 @@
+import {LitElement, html, css} from 'lit-element';
+
+import '@polymer/paper-card/paper-card.js';
+import '@vaadin/vaadin-text-field/vaadin-text-field.js';
+import '@vaadin/vaadin-text-field/vaadin-password-field.js';
+import '@vaadin/vaadin-button/vaadin-button.js';
+
+export class IBLogin extends LitElement {
+    // En el constructor es donde se declaran las propiedades del elemento. (Equivalente a "state" en React).
+    constructor() {
+        super();
+        this.logo = "";
+        this.username = "Fernando";
+        this.password = "1234";
+    }
+
+    // Para agregar estilos creamos un metodo estático que devuelva un String con los estilos personalizados.
+    static get styles() {
+        return css`
+            .container {
+                width: 350px;
+                margin-left: auto;
+                margin-right: auto;
+                font-family: 'Montserrat', sans-serif;
+            }
+
+            .card-actions {
+                display: flex;
+                flex-direction: column;
+            }
+        `;
+    }
+
+    // Metodo para devolver las propiedades.
+    static get properties() {
+        return {
+            logo: String,
+            usename: String,
+            password: String
+        }
+    }
+
+    render() {
+        return html`
+        <div class="container">
+            <paper-card image="https://avatars3.githubusercontent.com/u/29693607?s=460&v=4" alt="Emmental">
+                <div class="card-content">
+                    <h1>Fernando Jimenez</h1>
+                </div>
+                <div class="card-actions">
+                    <vaadin-text-field label="Username"></vaadin-text-field>
+                    <vaadin-password-field label="Password"></vaadin-password-field>
+                    <vaadin-button theme="primary">Login</vaadin-button>
+                </div>
+            </paper-card>
+        </div>
+        `;
+    }
+}
+
+window.customElements.define('ib-login', IBLogin);
