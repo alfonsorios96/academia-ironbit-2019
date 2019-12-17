9b1dcbad0ced1e9ecbcaf571230b1bab1d9caa8b
ErickSerrato <Erick.Serrato@ironbit.com.mx>
2019-12-17 16:52:41 -0600

    fix (correccionanotacion)

--- ErickSerratoIronbit/practica12 -12 -2019/validatorsudoku.js
+++ ErickSerratoIronbit/practica12 -12 -2019/validatorsudoku.js
@@ -27,24 +27,8 @@ const makeGroups =()=> {
   for(let value of grid[0]){
     group.push(new Set());
   }
-  //for(let i=0; i<9; i++) {
-  //  group.push(new Set());
-  //}
   return group;
 }
-
-const grid = [
-  [5, 3, 4, 6, 7, 8, 9, 1, 2],
-  [6, 7, 2, 1, 9, 5, 3, 4, 8],
-  [1, 9, 8, 3, 4, 2, 5, 6, 7],
-  [8, 5, 9, 7, 6, 1, 4, 2, 3],
-  [4, 2, 6, 8, 5, 3, 7, 9, 1],
-  [7, 1, 3, 9, 2, 4, 8, 5, 6],
-  [9, 6, 1, 5, 3, 7, 2, 8, 4],
-  [2, 8, 7, 4, 1, 9, 6, 3, 5],
-  [3, 4, 5, 2, 8, 6, 1, 7, 9]
- ];
-
 const locateGrid = (row, column) =>{
   const gridCol = Math.floor(column/3);
   const gridRow = Math.floor(row/3);
