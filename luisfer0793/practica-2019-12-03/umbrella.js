f5b338bafae6c0c1de6f5e8505f666831f9e2147
Luis Fernando Jimenez Lopez <luisfer.0793@gmail.com>
2019-12-17 18:00:41 -0600

    Checked some options

--- luisfer0793/practica-2019-12-03/umbrella.js
+++ luisfer0793/practica-2019-12-03/umbrella.js
@@ -1,18 +1,15 @@
 const options = ["clear", "sunny", "cloudy", "rainy", "windy", "thunderstorms"];
 
-function minUmbrellas(weather) {
-    // Debemos de saber si hay o no al menos un paraguas en casa o en oficina, por lo tanto, si esta lloviendo y no tenemos paraguas agregaremos uno nuevo
-    let atHome = 0, 
-        atOffice = 0;
-    const laps = weather.map((forecast, weatherIndex) => {
-        // Para determinar si usamos paraguas o no, debemos de comprobar si el clima es 'rainy' o 'thunderstorms'.
-        return ({
-            clima: weather,
-            estado: weatherIndex % 2 === 0 ? 'IDA' : 'VUELTA',
-            useAnUmbrella: forecast === 'rainy' || forecast === 'thunderstorms' ? true : false
-        });
-    });
-            
-    const umbrellas = laps.reduce((acc, current) => {}, 0);
+const minUmbrellas = weather => {
+    // Caso que NO llueva.
+    if ( !(weather.includes('rainy') || weather.includes('thunderstorms')) ) {
+        return 0;
+    }
+    // Caso que TODOS los días llueva.
+    if (weather.every(current => current === 'rainy' || current === 'thunderstorms')) {
+        return 1;
+    }
+    // Caso que llueva en ALGÚN día.
+    let atHome = 1, atOffice = 1;
 }
-console.log(atHome, atOffice);
+console.log(minUmbrellas(every));
