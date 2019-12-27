398140752557008b4e095dbf85769a9c9ee17f68
Fernando <luisfer.0793@gmail.com>
2019-12-26 23:02:17 -0600

    Improved the solution

--- luisfer0793/practica-2019-12-03/umbrella.js
+++ luisfer0793/practica-2019-12-03/umbrella.js
@@ -1,15 +1,25 @@
 const options = ["clear", "sunny", "cloudy", "rainy", "windy", "thunderstorms"];
 
 const minUmbrellas = weather => {
-    // Caso que NO llueva.
     if ( !(weather.includes('rainy') || weather.includes('thunderstorms')) ) {
         return 0;
     }
-    // Caso que TODOS los días llueva.
     if (weather.every(current => current === 'rainy' || current === 'thunderstorms')) {
         return 1;
     }
-    // Caso que llueva en ALGÚN día.
-    let atHome = 1, atOffice = 1;
+    let atHome = 0, atOffice = 0;
+    return weather.reduce((accumulator, current, index) => {
+        if (index % 2 === 0) {
+            if (atOffice === 0) {
+                atOffice++;
+                return accumulator++;
+            }
+        }
+        else {
+            if (atHome === 0) {
+                atHome++;
+                return accumulator++;
+            }
+        }
+    }, 0);
 }
-console.log(minUmbrellas(every));
